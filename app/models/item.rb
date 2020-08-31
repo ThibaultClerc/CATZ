6ab837870313bcf2a6a8e94a472f36e03c08ce9a
Damien <damienbanville@hotmail.fr>
2020-08-31 16:22:55 +0200

    [new] migrations and models related to Cart and its JoinTable

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :image_url, presence: true
+  has_many :join_table_item_carts
+  has_many :carts, through: :join_table_item_carts
 end
