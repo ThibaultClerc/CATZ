5fbb5ff598c5b88693e7b2f7e4fd6ae1bfcc7587
ThibaultClerc <thibault.clerc2@gmail.com>
2020-09-01 18:31:20 +0200

    stripe ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :carts
-  
+  resources :orders
   resources :items
   root to: "items#index"
 end
