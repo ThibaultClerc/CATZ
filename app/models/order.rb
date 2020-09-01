3d4b46429b63aa1d6826ff8ba97b5d35397fe996
Inigo-santiu <dstavroff@gmail.com>
2020-09-01 15:09:18 +0200

    links between item-user-order tables

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  # validates :item_name, presence: true
+  # validates :price, presence: true
+  # validates :quantity, presence: true
+  # validates :payment_method, presence: true
+  belongs_to :user
+  has_many :join_table_item_orders
+  has_many :item, through: :join_table_item_orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_083226) do
-
+ActiveRecord::Schema.define(version: 2020_09_01_124227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "orders", force: :cascade do |t|
-    t.string "item_name"
-    t.integer "price"
-    t.integer "quantity"
-    t.string "payment_method"
-    t.string "client_name"
-    t.string "client_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
@@ -52,6 +40,25 @@ ActiveRecord::Schema.define(version: 2020_09_01_083226) do
     t.index ["item_id"], name: "index_join_table_item_carts_on_item_id"
   end
 
+  create_table "join_table_item_orders", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_join_table_item_orders_on_item_id"
+    t.index ["order_id"], name: "index_join_table_item_orders_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "price"
+    t.integer "quantity"
+    t.string "payment_method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -69,4 +76,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_083226) do
   add_foreign_key "carts", "users"
   add_foreign_key "join_table_item_carts", "carts"
   add_foreign_key "join_table_item_carts", "items"
-
+  add_foreign_key "join_table_item_orders", "items"
+  add_foreign_key "join_table_item_orders", "orders"
+  add_foreign_key "orders", "users"
+end
